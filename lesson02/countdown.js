e8e6e1f5504cc9b25e3d32cac4ff9f7c07f6481e
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-03-28 20:40:16 -0700

    Added countdown.js and new branch

---
+++ lesson02/countdown.js
@@ -0,0 +1,10 @@
+// Create a single loop that console logs a variable number (✔️)
+// Decrease the variable by one each loop (✔️)
+// Stop once log outputs 0 (✔️)
+
+let x = 15;
+
+while (x >= 0) {
+  console.log(x);
+  x--;
+}
