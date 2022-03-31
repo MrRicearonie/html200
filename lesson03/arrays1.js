8a0932dc2f19631cd07701b0a5ddda416ae0eaec
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-03-31 16:13:50 -0700

    Completed assignment 1

---
+++ lesson03/arrays1.js
@@ -0,0 +1,8 @@
+const arr = ['eins', 'zwei', 'drei', 'vier', 'fünf'];
+console.log(arr);
+arr.push('sechs');
+console.log(arr);
+arr.splice(2, 1);
+console.log(arr);
+const str = arr.join(',');
+console.log(str);
